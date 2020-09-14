d6fb5e67137a5a72cc12a10cd97a244bf8406254
Schmiesing <Wolfgang.Schmiesing@tk.de>
2020-09-14 12:48:47 +0200

    update groupid

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-group = "de.tk.security"
+group = "de.tk.opensource"
 version = "0.0.1-SNAPSHOT"
 
 application {
