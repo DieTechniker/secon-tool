0f672091d67e67f0177c2795af94f26f4f7d359d
Schmiesing <Wolfgang.Schmiesing@tk.de>
2020-09-17 11:38:16 +0200

    changed license to LGPLv3

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,17 +2,21 @@
  * Copyright © 2020 Techniker Krankenkasse
  * Copyright © 2020 BITMARCK Service GmbH
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This file is part of kks-encryption
+ * (see https://github.com/DieTechniker/kks-encryption).
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 group = "de.tk.opensource"
 version = "0.0.1-SNAPSHOT"
@@ -36,6 +40,7 @@ plugins {
     `java-library`
     `maven-publish`
     id("com.github.johnrengelman.shadow") version "6.0.0"
+	id("net.minecrell.licenser") version "0.4.1"
 }
 
 repositories {
@@ -81,6 +86,12 @@ tasks.register<Test>("testLdap") {
     useJUnitPlatform {
         includeTags("LDAP")
     }
+}
+
+license {
+	header = project.file("LICENSE.header")
+	include("**/*.java,**/*.kts")
+	newLine = false
 }
 
 publishing {
