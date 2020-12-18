463665f79a78eea7a074cb5c47d1e6316874ed6a
Christian Schlichtherle <christian@schlichtherle.de>
2020-12-18 11:27:02 +0100

    Improve test coverage.

--- src/test/java/de/tk/opensource/secon/LdapConnectionTest.java
+++ src/test/java/de/tk/opensource/secon/LdapConnectionTest.java
@@ -26,18 +26,21 @@ import reactor.test.StepVerifier;
 import java.net.URI;
 import java.security.cert.X509Certificate;
 import java.time.Duration;
+import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Function;
 
 import static de.tk.opensource.secon.SECON.directory;
 import static java.util.Locale.ENGLISH;
-import static java.util.Objects.requireNonNull;
 import static org.junit.jupiter.api.Assertions.assertTimeoutPreemptively;
-import static reactor.core.scheduler.Schedulers.newParallel;
+import static reactor.core.scheduler.Schedulers.parallel;
 
 @Tag("LDAP")
 public class LdapConnectionTest {
 
     private static final Duration timeout = Duration.ofSeconds(3);
+
+    private static final int NUM_PROC = Runtime.getRuntime().availableProcessors();
+    private static final int MAX_CERTS = 32 * NUM_PROC;
 
     // See https://kkv.gkv-diga.de as of December 17th, 2020, plus non-existing 999_999_999:
     private static final Flux<String> ids = Flux.just(
@@ -46,8 +49,6 @@ public class LdapConnectionTest {
             104_940_005, 105_810_615, 107_299_005, 107_310_373, 108_018_007,
             108_310_400, 109_519_005, 109_905_003, 660_500_345, 999_999_999
     ).map(id -> String.format(ENGLISH, "%09d", id));
-
-    private static final int idCount = requireNonNull(ids.count().block()).intValue();
 
     private final Directory dir = directory(URI.create("ldap://localhost"));
 
@@ -60,10 +61,18 @@ public class LdapConnectionTest {
     }
 
     @Test
-    void parallelCertCount() {
+    void parallelCertCountFew() {
         assertTimeoutPreemptively(
                 timeout,
-                () -> StepVerifier.create(countCertsParallel()).expectNextCount(1).verifyComplete()
+                () -> StepVerifier.create(countFewCertsParallel()).expectNextCount(1).verifyComplete()
+        );
+    }
+
+    @Test
+    void parallelCertCountMany() {
+        assertTimeoutPreemptively(
+                timeout,
+                () -> StepVerifier.create(countManyCertsParallel()).expectNextCount(1).verifyComplete()
         );
     }
 
@@ -73,12 +82,24 @@ public class LdapConnectionTest {
                 .count();
     }
 
-    private Mono<Long> countCertsParallel() {
+    private Mono<Long> countFewCertsParallel() {
         return ids
-                .parallel(idCount)
-                .runOn(newParallel("ldap", idCount, true))
+                .parallel(NUM_PROC)
+                .runOn(parallel())
                 .flatMap(this::cert)
                 .sequential()
+                .count();
+    }
+
+    private Mono<Long> countManyCertsParallel() {
+        return Flux
+                .<String>generate(ss -> ss.next(randomIdentifier()))
+                .take(MAX_CERTS)
+                .parallel(NUM_PROC)
+                .runOn(parallel())
+                .flatMap(this::cert)
+                .sequential()
+//                .log()
                 .count();
     }
 
@@ -87,6 +108,10 @@ public class LdapConnectionTest {
                 .just(identifier)
                 .flatMap(unchecked(dir::certificate))
                 .flatMap(Mono::justOrEmpty);
+    }
+
+    private static String randomIdentifier() {
+        return String.format(ENGLISH, "58%07d", ThreadLocalRandom.current().nextInt(10_000_000));
     }
 
     private static <T, R> Function<T, Mono<R>> unchecked(XFunction<T, R> f) {
