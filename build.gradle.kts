4467ad8452c661f6870208e402b98de11ccba142
Wolfgang Schmiesing <wolfgang.schmiesing@googlemail.com>
2022-01-18 20:35:04 +0100

    update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 group = "de.tk.opensource"
-version = "1.1.1-SNAPSHOT"
+version = "1.1.1"
 
 application {
     mainClassName = "de.tk.opensource.secon.Main"
