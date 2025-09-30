17604c90063154065aff43dcb400f53cd6152ded
Wolfgang Schmiesing <wolfgang.schmiesing@googlemail.com>
2025-09-30 21:39:46 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -107,8 +107,8 @@ nexusPublishing {
         sonatype {
             nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
             snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
-            username = System.getenv("MAVEN_USERNAME")
-            password = System.getenv("MAVEN_PASSWORD")
+            username.set(System.getenv("MAVEN_USERNAME"))
+            password.set(System.getenv("MAVEN_PASSWORD"))
         }
     }
 }
