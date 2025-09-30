ad24ef1484b6bcbf787f9e8844de7969fc8a4551
Wolfgang Schmiesing <wolfgang.schmiesing@googlemail.com>
2025-09-30 09:48:07 +0200

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,7 +19,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 group = "de.tk.opensource"
-version = "1.2.1-SNAPSHOT"
+version = "1.2.1"
 
 application {
     mainClassName = "de.tk.opensource.secon.Main"
