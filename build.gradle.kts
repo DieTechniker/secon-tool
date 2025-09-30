7d26e57c26dee41d6993292b18cfccff33c836ec
Wolfgang Schmiesing <wolfgang.schmiesing@googlemail.com>
2025-09-30 21:46:39 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -101,9 +101,6 @@ license {
 
 nexusPublishing {
     repositories {
-        sonatype()
-    }
-    repositories {
         // see https://central.sonatype.org/publish/publish-portal-ossrh-staging-api/#configuration
         sonatype {
             nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
